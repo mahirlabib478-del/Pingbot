593de089ff6b5ed072cc05312d51b295e3d9e406
mahirlabib478-del <mahirlabib478@gmail.com>
2026-07-15 21:03:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 # ================== CONFIG (ENV VARS) ==================
-BOT_TOKEN = "8851844397:AAHMc_7HqSqlj3YH8Rz2jxm2NbwbUzBPh-w"
+BOT_TOKEN = "8851844397:AAEyO8xXQGCT_uxtxnLHsIrJHlzXMSLkI0g"
 ADMIN_CHAT_ID = "2035024902"
 CHANNEL_ID = "-1003903695158"
 BOT_USERNAME = "instagramworksbot"
